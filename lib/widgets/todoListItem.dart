cfeb755f79563bf544e9f0ff1f226a7b06a3068e
projEduardoValencio <108006820+projEduardoValencio@users.noreply.github.com>
2022-07-04 20:04:10 -0300

    add slidable

--- lib/widgets/todoListItem.dart
+++ lib/widgets/todoListItem.dart
@@ -4,6 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:lista_de_tarefas/models/todo_model.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class TodoListItem extends StatelessWidget {
   const TodoListItem({Key? key, required this.todo}) : super(key: key);
@@ -14,48 +15,71 @@ class TodoListItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(5),
-          color: Colors.grey[300],
-        ),
-        height: 60,
-        child: Padding(
-          padding: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
-          child: Row(
-            children: [
-              SizedBox(
-                width: 2,
-              ),
-              //CHECKBOX
-              Checkbox(value: false, onChanged: (_) {}),
-              SizedBox(
-                width: 10,
-              ),
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    //TODO TITLE
-                    Text(
-                      "${todo.title}",
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                    ),
-                    // SizedBox(
-                    //   height: 1,
-                    // ),
-                    //TODO DATE
-                    Text(
-                      DateFormat('dd/MMM/yyyy - HH:mm').format(todo.date),
-                      style: TextStyle(color: Colors.grey[600], fontSize: 12),
-                    ),
-                  ],
-                ),
-              ),
-            ],
+      child: Slidable(
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5),
+            color: Colors.grey[300],
           ),
+          height: 60,
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+            child: Row(
+              children: [
+                SizedBox(
+                  width: 2,
+                ),
+                //CHECKBOX
+                Checkbox(value: false, onChanged: (_) {}),
+                SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      //TODO TITLE
+                      Text(
+                        "${todo.title}",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 16),
+                      ),
+                      // SizedBox(
+                      //   height: 1,
+                      // ),
+                      //TODO DATE
+                      Text(
+                        DateFormat('dd/MMM/yyyy - HH:mm').format(todo.date),
+                        style: TextStyle(color: Colors.grey[600], fontSize: 12),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        endActionPane: ActionPane(
+          motion: DrawerMotion(),
+          extentRatio: 0.20,
+          children: const [
+            // A SlidableAction can have an icon and/or a label.
+            SlidableAction(
+              onPressed: null,
+              backgroundColor: Color(0xFFFE4A49),
+              foregroundColor: Colors.white,
+              icon: Icons.delete,
+              label: 'Delete',
+            ),
+            // SlidableAction(
+            //   onPressed: null,
+            //   backgroundColor: Color(0xFF21B7CA),
+            //   foregroundColor: Colors.white,
+            //   icon: Icons.share,
+            //   label: 'Share',
+            // ),
+          ],
         ),
       ),
     );
